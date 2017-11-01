7cee565c89dd3a2881a465d0243751bf0dd38171
[yrjxqy3] <[yrjxqy3@126.com]>
2017-11-01 11:31:27 +0800

    tianjia

---
+++ application/controllers/Welcome.php
@@ -0,0 +1,15 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Welcome extends CI_Controller {
+
+function Welcome(){
+	parent::__construct();
+	$this->load->helper('url');
+}
+
+	public function index()
+	{
+		$this->load->view('welcome_message');
+	}
+}
